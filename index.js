731924f299bf5c38d249750dcccc497ccca02781
LongBody <54003924+LongBody@users.noreply.github.com>
2020-06-24 20:45:17 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 var bodyParser = require('body-parser')
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
-const port = 5698 || process.env.PORT
+
 
 app.set("view engine" , "ejs")
 app.set("views" ,"./views")
@@ -78,7 +78,7 @@ io.on('connection', (socket) => {
   });
 
   
-http.listen(port, () => {
+http.listen(process.env.PORT || 5000, () => {
   console.log('listening on *:3000');
 });
 
